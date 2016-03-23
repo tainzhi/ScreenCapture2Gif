52a4858ee33891ee2a983615532679be7857b485
qiufuqing <qfq0601@gmail.com>
2016-03-24 07:52:02 +0800

    to record screen

--- app/src/main/java/com/muqing/android/screencapture2gif/notification/ScreenRecordNotification.java
+++ app/src/main/java/com/muqing/android/screencapture2gif/notification/ScreenRecordNotification.java
@@ -2,10 +2,13 @@ package com.muqing.android.screencapture2gif.notification;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
 import com.muqing.android.screencapture2gif.activity.SettingsActivity;
+import com.muqing.android.screencapture2gif.service.ScreenCaptureService;
 import com.muqing.android.screencapture2gif.util.MyConstants;
 
 /**
@@ -17,7 +20,8 @@ public class ScreenRecordNotification {
     private Context mContext;
     private NotificationManager mNotificationManager;
     private NotificationCompat.Builder mBuilder;
-    private static final int mNotifyId = 1;
+    private static final int sNotifyId = 1;
+    private static final String ACTION_STOP_SERVICE = "STOP_SERVICE";
 
     public ScreenRecordNotification(Context context) {
         mContext = context;
@@ -29,9 +33,13 @@ public class ScreenRecordNotification {
                 .setContentTitle("apple")
                 .setContentText("pear")
                 .setOngoing(true)
+                .setAutoCancel(true)
                 .setSmallIcon(android.support.v7.appcompat.R.drawable.notification_template_icon_bg);
-
-        mNotificationManager.notify(mNotifyId, mBuilder.build());
+        Intent intent = new Intent(mContext, ScreenCaptureService.class);
+        intent.setAction(ACTION_STOP_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getService(mContext, 0, intent, 0);
+        mBuilder.setContentIntent(pendingIntent);
+        mNotificationManager.notify(sNotifyId, mBuilder.build());
     }
 
 }
--- app/src/main/java/com/muqing/android/screencapture2gif/service/ScreenCaptureService.java
+++ app/src/main/java/com/muqing/android/screencapture2gif/service/ScreenCaptureService.java
@@ -3,8 +3,11 @@ package com.muqing.android.screencapture2gif.service;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjectionManager;
 import android.os.*;
 import android.os.Process;
+import android.util.DisplayMetrics;
 import android.util.Log;
 
 import com.muqing.android.screencapture2gif.notification.ScreenRecordNotification;
@@ -17,7 +20,13 @@ public class ScreenCaptureService extends Service {
     private Looper mServiceLooper;
     private ServiceHandler mServiceHandler;
 
+    private int mScreenDensity;
+    private MediaRecorder mMediaRecorder;
+    private MediaProjectionManager mMediaProjectionManager;
+
     private static final int MSG_START_CAPTURE = 1;
+    private static final int MSG_STOP_SERVICE = 2;
+
 
     private final class ServiceHandler extends Handler {
         public ServiceHandler(Looper looper) {
@@ -31,6 +40,10 @@ public class ScreenCaptureService extends Service {
                     Log.v(TAG, "handleMessage");
                     ScreenRecordNotification screenRecordNotification = new ScreenRecordNotification(mContext);
                     screenRecordNotification.startNotificate();
+                    break;
+                case MSG_STOP_SERVICE:
+                    Log.v(TAG, "stop service");
+                    stopSelf();
                 default:
                     break;
             }
@@ -43,6 +56,11 @@ public class ScreenCaptureService extends Service {
     public void onCreate() {
         Log.v(TAG, "onCreate");
         mContext = getApplicationContext();
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowmanager().getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity = metrics.densityDpi;
+
         HandlerThread thread = new HandlerThread("ScreenCaptureService handler",
                 Process.THREAD_PRIORITY_BACKGROUND);
         thread.start();
@@ -54,14 +72,28 @@ public class ScreenCaptureService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.v(TAG, "onStart");
-        Message msg = mServiceHandler.obtainMessage();
-        msg.what = MSG_START_CAPTURE;
-        mServiceHandler.sendMessage(msg);
+
+        String action = intent.getAction();
+        if (action != null && action.equals("STOP_SERVICE")) {
+            Message msg = mServiceHandler.obtainMessage();
+            msg.what = MSG_STOP_SERVICE;
+            mServiceHandler.sendMessage(msg);
+        } else {
+            Message msg = mServiceHandler.obtainMessage();
+            msg.what = MSG_START_CAPTURE;
+            mServiceHandler.sendMessage(msg);
+        }
         return START_NOT_STICKY;
     }
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.v(TAG, "onDestroy");
+        super.onDestroy();
     }
 }
